8ab80e2ff50644830cff5a1f9b94ff54a959af38
Satshabad Khalsa <satshabad.music@gmail.com>
2013-03-28 16:35:32 -0700

    init db needs def

--- queue.py
+++ queue.py
@@ -5,12 +5,16 @@ from flask.ext.sqlalchemy import SQLAlchemy
 
 API_URL = "http://ws.audioscrobbler.com/2.0/?"
 API_KEY = "7caf7bfb662c58f659df4446c7200f3c&"
-SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/queue.db'
+DATABASE_FILE = '/tmp/queue.db'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///%s' % DATABASE_FILE
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 api = Api(app)
 db = SQLAlchemy(app)
 
-if __name__ == "__main__":
+def init_db():
     db.create_all()
+
+if __name__ == "__main__":
+    init_db()
