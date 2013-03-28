fad36562cbc489a9c2a38057630ff272552b8b53
Satshabad Khalsa <satshabad.music@gmail.com>
2013-03-28 11:10:27 -0700

    adding to queue now working

--- models.py
+++ models.py
@@ -1,14 +1,43 @@
+import calendar
+import json
+
 import sqlalchemy
+from sqlalchemy.ext.declarative import declarative_base
+
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship, backref
+
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.types import DateTime, Boolean
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+
+engine = create_engine('sqlite:////tmp/queue.db', convert_unicode=True)
+db_session = scoped_session(sessionmaker(autocommit=False,
+                                         autoflush=False,
+                                         bind=engine))
+Base = declarative_base(engine)
+
 class User(Base):
     __tablename__ = 'users'
+
+    def __init__(self, name, auth):
+        self.uname = name
+        self.auth = auth
 
     id = Column(Integer, primary_key=True)
     uname = Column(String)
     fullname = Column(String)
     image_link = Column(String)
+    auth = Column(String)
+
+    def dictify(self):
+        return {'user_name':self.uname,
+                'full_name':self.fullname,
+                'image':self.image_link}
 
     def __repr__(self):
         return "<User('%s','%s', '%s')>" % (self.uname, self.fullname, self.image_link)
@@ -16,43 +45,88 @@ class User(Base):
 class Song(Base):
     __tablename__ = 'songs'
 
-    id = Column(String, primary_key=True)
-    rank = Column(Integer)
+    id = Column(Integer, primary_key=True)
+    listened = Column(Boolean)
+    spotifylink = Column(String)
+    date_queued = Column(DateTime)
 
-    user_id = Column(Integer, ForeignKey('users.id')
-    user = relationship("User", backref=backref('songs', order_by=rank))
+    user_id = Column(Integer, ForeignKey('users.id'))
+    user = relationship("User", foreign_keys=[user_id], backref=backref('queue', order_by=date_queued))
+
+    queued_by_id = Column(Integer, ForeignKey('users.id'))
+    queued_by_user = relationship("User", foreign_keys=[queued_by_id], backref=backref('sent', order_by=date_queued))
 
     artist_id = Column(Integer, ForeignKey('artists.id'))
     artist = relationship("Artist", backref=backref('songs', order_by=id))
 
     album_id = Column(Integer, ForeignKey('albums.id'))
-    album = relationship("Albums", backref=backref('songs', order_by=id))
+    album = relationship("Album", backref=backref('songs', order_by=id))
 
     name = Column(String)
-    date = Column(DateTime)
 
     small_image_link = Column(String)
     medium_image_link = Column(String)
     large_image_link = Column(String)
 
+    def dictify(self):
+        return {'listened':self.listened,
+                'from_user': self.queued_by_user.dictify(),
+                'artist': self.artist.dictify(),
+                'album': self.album.dictify(),
+                'name': self.name,
+                'date_queued':calendar.timegm(self.date_queued.utctimetuple()),
+                'size':{
+                        'small':self.small_image_link,
+                        'medium':self.medium_image_link,
+                        'large':self.large_image_link
+                  }}
+
+
     def __repr__(self):
-        return "<Song('%s', '%s','%s', '%s')>" % (self.rank, self.name, self.artist, self.album)
+        return "<Song('%s','%s', '%s', '%s')>" % (self.name, self.artist, self.album, self.user_id)
 
 class Artist(Base):
     __tablename__ = 'artists'
 
-    id = Column(String, primary_key=True)
+    id = Column(Integer, primary_key=True)
     name = Column(String)
+    mbid = Column(String)
+
+    small_image_link = Column(String)
+    medium_image_link = Column(String)
+    large_image_link = Column(String)
+
+    def dictify(self):
+        return {
+                'name':self.name,
+                'mbid':self.mbid,
+                'size':{
+                        'small':self.small_image_link,
+                        'medium':self.medium_image_link,
+                        'large':self.large_image_link
+                    }}
+
 
     def __repr__(self):
         return "<Artist('%s','%s')>" % (self.name, self.id)
 
 
 class Album(Base):
-    __tablename__ = 'album'
+    __tablename__ = 'albums'
 
-    id = Column(String, primary_key=True)
+    id = Column(Integer, primary_key=True)
     name = Column(String)
+    mbid = Column(String)
+
+    def dictify(self):
+        return {
+                'name':self.name,
+                'mbid':self.mbid
+                }
+
 
     def __repr__(self):
-        return "<Artist('%s','%s')>" % (self.name, self.id)
+        return "<Album('%s','%s')>" % (self.name, self.id)
+
+
+Base.metadata.create_all(engine)
--- queueapi.py
+++ queueapi.py
@@ -1,7 +1,13 @@
+import datetime
+import json
+
 from flask import Flask, request
 from flask.ext.restful import Resource, Api, reqparse
-
 import requests
+
+import models
+from models import Song, User, Artist, Album
+from models import db_session
 
 API_URL = "http://ws.audioscrobbler.com/2.0/?"
 API_KEY = "7caf7bfb662c58f659df4446c7200f3c&"
@@ -12,7 +18,8 @@ api = Api(app)
 parser = reqparse.RequestParser()
 parser.add_argument('song', type=dict)
 parser.add_argument('auth', type=str)
-parser.add_argument('to_user_id', type=str)
+parser.add_argument('from_user_id', type=str)
+parser.add_argument('default', type=str)
 
 def fix_lastfm_data(data):
     data['recenttracks'][u'metadata'] = data['recenttracks'].pop('@attr')
@@ -21,11 +28,19 @@ def fix_lastfm_data(data):
     for i, track in enumerate(data['recenttracks']['tracks']):
 
         track['album'][u'name'] = track['album'].pop('#text')
-
+        track['streamable'] = bool(int(track['streamable']))
+        track['loved'] = bool(int(track['loved']))
 
         del track['artist']['url']
 
-        del track['date']['#text']
+        if track.has_key("date"):
+            del track['date']['#text']
+            track.update(track["date"])
+            del track['date']
+
+        if track.has_key("@attr"):
+            track['nowplaying'] = True
+            del track["@attr"]
 
         track[u'images'] = track.pop('image')
 
@@ -44,42 +59,97 @@ def fix_lastfm_data(data):
 
 
 
-
-
 def get_args(args):
     pass
 
 class Listens(Resource):
-    def get(self, user_id):
-        data = requests.get("%smethod=user.getrecenttracks&user=%s&api_key=%sformat=json&extended=1" % (API_URL, user_id, API_KEY)).json()
+    def get(self, user_name):
+        data = requests.get("%smethod=user.getrecenttracks&user=%s&api_key=%sformat=json&extended=1" % (API_URL, user_name, API_KEY)).json()
         return fix_lastfm_data(data)
 
 
 class Friends(Resource):
-    def get(self, user_id):
+    def get(self, user_name):
         args = parser.parse_args()
         auth = map(args.get, ['auth'])
         pass
 
-class User(Resource):
-    def post(self, user_id):
-        args = parser.parse_args()
+class UserAPI(Resource):
+    def post(self, user_name):
+        args = request.json
         auth, default = map(args.get, ['auth', 'default'])
+        u = User(user_name, auth)
+        db_session.add(u)
+        db_session.commit()
+
+        return {"status":"OK"}
 
 
 class Queue(Resource):
-    def get(self, user_id):
-        pass
+    def get(self, user_name):
+        user_id = db_session.query(User.id).filter(User.uname == user_name).one()[0]
+        orm_songs = db_session.query(Song).filter(Song.user_id == user_id).all()
 
-    def post(self, from_user_id):
-        args = parser.parse_args()
-        auth, song, to_user_id= map(args.get, ['auth', 'song', 'to_user_id'])
+        for song in db_session.query(Song):
+            print song
+        songs = []
 
-api.add_resource(Listens, '/<string:user_id>/listens')
-api.add_resource(Friends, '/<string:user_id>/friends')
-api.add_resource(User, '/<string:user_id>')
-api.add_resource(Queue, '/<string:user_id>/queue')
+        for orm_song in db_session.query(Song):
+            songs.append(orm_song.dictify())
+
+        return {"queue":songs}
+
+    def post(self, user_name):
+
+        args = request.json
+        auth, song, from_user_id = map(args.get, ['auth', 'song', 'from_user_id'])
+        from_user = db_session.query(User).filter(User.uname == from_user_id).one()
+        to_user = db_session.query(User).filter(User.uname == user_name).one()
+
+        if from_user and to_user:
+
+            artist = song['artist']
+            orm_artist = Artist(name=artist['name'], mbid=artist['mbid'],
+                                small_image_link=artist['image']['small'],
+                                medium_image_link=artist['image']['small'],
+                                large_image_link=artist['image']['small'])
+
+            album = song['album']
+            orm_album = Album(name=album['name'], mbid=album['mbid'])
+
+            orm_song = Song(user=to_user,queued_by_user=from_user,
+                            listened=False, name=song['name'],
+                            date_queued=datetime.datetime.utcnow(),
+                            small_image_link=song['image']['small'],
+                                medium_image_link=song['image']['small'],
+                                large_image_link=song['image']['small'])
+
+            orm_song.artist = orm_artist
+            orm_song.album = orm_album
+            db_session.add(orm_song)
+            db_session.add(orm_album)
+            db_session.add(orm_artist)
+
+            db_session.commit()
+
+            return {"status":"OK"}
+        return {"status":"Not OK"}
+
+
+
+
+
+api.add_resource(Listens, '/<string:user_name>/listens')
+api.add_resource(Friends, '/<string:user_name>/friends')
+api.add_resource(UserAPI, '/<string:user_name>')
+api.add_resource(Queue, '/<string:user_name>/queue')
+
+
+@app.teardown_request
+def shutdown_session(exception=None):
+    db_session.remove()
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', debug=True)
 
