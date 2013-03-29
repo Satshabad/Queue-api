95536141afe9f1ff5a71d20479763eba633a4b08
Satshabad Khalsa <satshabad.music@gmail.com>
2013-03-29 12:08:51 -0700

    added fb url, changed names

--- queue.py
+++ queue.py
@@ -3,8 +3,9 @@ from flask.ext.restful import Resource, Api, reqparse
 from flask.ext.sqlalchemy import SQLAlchemy
 
 
-API_URL = "http://ws.audioscrobbler.com/2.0/?"
-API_KEY = "7caf7bfb662c58f659df4446c7200f3c&"
+LF_API_URL = "http://ws.audioscrobbler.com/2.0/?"
+LF_API_KEY = "7caf7bfb662c58f659df4446c7200f3c&"
+FB_API_URL = "https://graph.facebook.com"
 DATABASE_FILE = '/tmp/queue.db'
 SQLALCHEMY_DATABASE_URI = 'sqlite:///%s' % DATABASE_FILE
 
