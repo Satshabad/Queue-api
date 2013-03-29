aca41ac09f62e273d39dcb26ba121c257cd5c7aa
Satshabad Khalsa <satshabad.music@gmail.com>
2013-03-29 12:08:37 -0700

    added more error handling

--- queueapi.py
+++ queueapi.py
@@ -8,10 +8,11 @@ import requests
 
 from queue import app, api, db
 
-from models import Song, User, Artist, Album
+from models import Song, User, Artist, Album, Friend
 
-API_URL = app.config['API_URL']
-API_KEY = app.config['API_KEY']
+LF_API_URL = app.config['LF_API_URL']
+LF_API_KEY = app.config['LF_API_KEY']
+FB_API_URL = app.config['FB_API_URL']
 
 def fix_lastfm_data(data):
     data['recenttracks'][u'metadata'] = data['recenttracks'].pop('@attr')
@@ -58,26 +59,43 @@ def fix_lastfm_data(data):
 
 class Listens(Resource):
     def get(self, user_name):
-        data = requests.get("%smethod=user.getrecenttracks&user=%s&api_key=%sformat=json&extended=1" % (API_URL, user_name, API_KEY)).json()
+        data = requests.get("%smethod=user.getrecenttracks&user=%s&api_key=%sformat=json&extended=1" % (LF_API_URL, user_name, LF_API_KEY)).json()
         return fix_lastfm_data(data)
 
 
 class Friends(Resource):
     def get(self, user_name):
-        args = request.json
-        auth = map(args.get, ['auth'])
-        pass
+        args = request.values
+        access_token = args['access_token']
+        user = get_user(user_name)
+
+        if not user:
+            return no_such_user(user_name)
+
+        friends = []
+        for friend in db.session.query(Friend).filter(Friend.user_id == user.id):
+            friends.append(friend.dictify())
+
+        return friends
 
 class UserAPI(Resource):
     def post(self, user_name):
         args = request.json
-        auth, default = map(args.get, ['auth', 'default'])
-        resp = requests.get("http://graph.facebook.com/%s/friends?limit=5000" % auth)
+        access_token = args['access_token']
+        default = args['default']
+        fb_id = args['fb_id']
+        resp = requests.get("%s/%s/friends?limit=5000&access_token=%s" %
+                                (FB_API_URL, fb_id, access_token))
+        if 'data' not in resp.json():
+            return {"status":500, "message": 'problem getting friends'}
+
         friends = resp.json()['data']
-        user = User(user_name, auth)
+        user = User(user_name, access_token)
         for friend in friends:
             f = Friend(friend['name'], friend['id'], user)
-        db.session.add(u)
+            db.session.add(f)
+
+        db.session.add(user)
         db.session.commit()
 
         return {"status":"OK"}
@@ -85,8 +103,12 @@ class UserAPI(Resource):
 
 class Queue(Resource):
     def get(self, user_name):
-        user_id = db.session.query(User).filter(User.uname == user_name).one().id
-        orm_songs = db.session.query(Song).filter(Song.user_id == user_id).all()
+
+        user = get_user(user_name)
+
+        if not user:
+            return no_such_user(user_name)
+        orm_songs = db.session.query(Song).filter(Song.user_id == user.id).all()
 
         songs = []
         for orm_song in orm_songs:
@@ -97,40 +119,67 @@ class Queue(Resource):
     def post(self, user_name):
 
         args = request.json
-        auth, song, from_user_name = map(args.get, ['auth', 'song', 'from_user_name'])
-        from_user = db.session.query(User).filter(User.uname == from_user_name).one()
-        to_user = db.session.query(User).filter(User.uname == user_name).one()
+        access_token = args['access_token']
+        song = args['song']
+        from_user_name = args['from_user_name']
 
-        if from_user and to_user:
+        from_user = get_user(from_user_name)
+        to_users = get_user(to_user_name)
 
-            artist = song['artist']
-            orm_artist = Artist(name=artist['name'], mbid=artist['mbid'],
-                                small_image_link=artist['images']['small'],
-                                medium_image_link=artist['images']['medium'],
-                                large_image_link=artist['images']['large'])
+        if not from_user:
+            return no_such_user(from_user)
 
-            album = song['album']
-            orm_album = Album(name=album['name'], mbid=album['mbid'])
+        if not to_user:
+            return no_such_user(to_user)
 
-            orm_song = Song(user=to_user,queued_by_user=from_user,
-                            listened=False, name=song['name'],
-                            date_queued=datetime.datetime.utcnow(),
-                            small_image_link=song['images']['small'],
-                                medium_image_link=song['images']['medium'],
-                                large_image_link=song['images']['large'])
+        if not is_friends(from_user, to_user):
+           return {'status':400, 'message':'users are not friends'}
 
-            orm_song.artist = orm_artist
-            orm_song.album = orm_album
-            db.session.add(orm_song)
-            db.session.add(orm_album)
-            db.session.add(orm_artist)
+        artist = song['artist']
+        orm_artist = Artist(name=artist['name'], mbid=artist['mbid'],
+                            small_image_link=artist['images']['small'],
+                            medium_image_link=artist['images']['medium'],
+                            large_image_link=artist['images']['large'])
 
-            db.session.commit()
+        album = song['album']
+        orm_album = Album(name=album['name'], mbid=album['mbid'])
 
-            return {"status":"OK"}
-        return {"status":"Not OK"}
+        orm_song = Song(user=to_user,queued_by_user=from_user,
+                        listened=False, name=song['name'],
+                        date_queued=datetime.datetime.utcnow(),
+                        small_image_link=song['images']['small'],
+                            medium_image_link=song['images']['medium'],
+                            large_image_link=song['images']['large'])
+
+        orm_song.artist = orm_artist
+        orm_song.album = orm_album
+        db.session.add(orm_song)
+        db.session.add(orm_album)
+        db.session.add(orm_artist)
+
+        db.session.commit()
+
+        return {"status":"OK"}
 
 
+def get_user(user_name):
+    users = list(db.session.query(User).filter(User.uname == user_name))
+    if not users:
+        return None
+
+    assert len(users) < 2
+    return users[0]
+
+def no_such_user(user_name):
+    return {"status":400, "message":"no such user %s" % user_name}
+
+def isFriends(user1, user2):
+    friends = list(db.session.query(Friend).filter(Friend.user_id == user1.id)\
+                                 .filter(Friend.user_id == user2.id))
+    if not friends:
+        return False
+
+    return True
 
 
 
