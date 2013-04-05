e896c09977b7faa943cfd4cd132710c490c71d89
Satshabad Khalsa <satshabad.music@gmail.com>
2013-04-04 17:00:57 -0700

    oops

--- queue.py
+++ queue.py
@@ -3,7 +3,6 @@ from flask.ext.restful import Resource, Api, reqparse
 from flask.ext.sqlalchemy import SQLAlchemy
 from raven.contrib.flask import Sentry
 
-sentry = Sentry(app, dsn='http://localhost:9000')
 LF_API_URL = "http://ws.audioscrobbler.com/2.0/?"
 LF_API_KEY = "7caf7bfb662c58f659df4446c7200f3c&"
 SP_API_URL = "http://ws.spotify.com"
@@ -13,6 +12,7 @@ SQLALCHEMY_DATABASE_URI = 'sqlite:///%s' % DATABASE_FILE
 
 app = Flask(__name__)
 app.config.from_object(__name__)
+sentry = Sentry(app, dsn='http://localhost:9000')
 api = Api(app)
 db = SQLAlchemy(app)
 
