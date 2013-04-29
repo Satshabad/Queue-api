e9656eecec03c36f3004722d651ccb86a2a70108
Satshabad Khalsa <satshabad.music@gmail.com>
2013-04-29 15:32:02 -0700

    well switching them didn't work out so well. I don't like python's imports

--- main.py
+++ main.py
@@ -22,6 +22,4 @@ try:
 except: ImportError
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
 
--- queueapi.py
+++ queueapi.py
@@ -303,6 +303,8 @@ def get_fb_friends(fb_id, access_token):
     return friends
     
 
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
 
 
 
