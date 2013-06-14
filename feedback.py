68746fc26353ebd9b8528a2907f5728aadd8afde
Satshabad Khalsa <satshabad.music@gmail.com>
2013-06-13 19:20:29 -0700

    full file path

--- feedback.py
+++ feedback.py
@@ -1,9 +1,12 @@
+import os
+
 from apnsclient import Session, APNs 
 from models import User
 from app import db
+
 def remove_tokens():
 # feedback needs no persistent connections.
-    con = Session.new_connection(("gateway.push.apple.com", 2196), cert_file="cert.pem", passphrase="this is the queue push key")
+    con = Session.new_connection(("gateway.push.apple.com", 2196), cert_file=os.path.join(os.getcwd(), 'cert.pem'), passphrase="this is the queue push key")
 
 # feedback server might be slow, so allow it to time out in 10 seconds
     srv = APNs(con, tail_timeout=10)
