d784650092fa070dcfe6f411654833b6f1924b5e
Satshabad Khalsa <satshabad.music@gmail.com>
2013-06-25 19:59:09 -0700

    make db more lasting perhaps...

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ TS_API_KEY = "6b08a682cc42b6eda827a1d1a9ab838a"
 TS_API_URL =  "http://tinysong.com"
 SP_API_URL = "http://ws.spotify.com"
 FB_API_URL = "https://graph.facebook.com"
-DATABASE_FILE = '/tmp/queue.db'
+DATABASE_FILE = 'queue.db'
 SQLALCHEMY_DATABASE_URI = 'sqlite:///%s' % DATABASE_FILE
 SECRET_KEY = "yeah, not actually a secret"
 PERMANENT_SESSION_LIFETIME = datetime.timedelta(minutes=525949)
