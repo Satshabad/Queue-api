1c5961f144623f2f604e1cb2effae6f40113679b
Satshabad Khalsa <satshabad.music@gmail.com>
2014-02-02 14:26:41 -0800

    fixed queue query

--- api/views/views.py
+++ api/views/views.py
@@ -127,7 +127,8 @@ def get_queue(user_id):
 
     items = QueueItem.query.filter(
         QueueItem.user_id == user.id).filter(
-        QueueItem.listened == 0).order_by(
+        QueueItem.listened == 0).filter(db.or_(
+        QueueItem.no_show == False, QueueItem.no_show == None)).order_by(
         QueueItem.date_queued.desc()).paginate(
         page, per_page=size, error_out=False).items
 
